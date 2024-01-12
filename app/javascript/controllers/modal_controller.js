d8e5473fb6227d18166087309d18fc59fc9172f9
Juan Lopez <capjuancode@gmail.com>
2024-01-12 22:26:19 +0100

    Implemented modal functionality and new ui

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,28 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [
-    'modal'
+    'modal',
+    'modalContent'
   ]
 
+  /**
+   * Shows the modal and the content associated with the clicked element.
+   *
+   * @param {Event} e - The click event.
+   */
   show(e) {
     e.preventDefault();
-
-    this.modalTarget.open = true
-    document.querySelector('.desktop-overlay').style.zIndex = 0
-    this.modalTarget.querySelectorAll('x-transition').forEach((transition) => {
-      transition.open = true
-    })
+    const { modalId } = e.currentTarget.dataset;
+    this.modalTarget.hidden = false;
+    this.modalContentTarget.querySelector(`#${modalId}`).hidden = false;
   }
 
+  /**
+   * Hides the modal and all its content.
+   *
+   * @param {Event} e - The click event.
+   */
   hide(e) {
-    e.preventDefault()
-
-    this.modalTarget.querySelectorAll('x-transition').forEach((transition) => {
-      transition.open = false
-    })
-    this.modalTarget.open = false
-
-    document.querySelector('.desktop-overlay').style.zIndex = 1
-    this.dispatch('closed')
+    e.preventDefault();
+    this.modalTarget.hidden = true;
+    this.modalContentTarget.childNodes.forEach(child => {
+      child.hidden = true;
+    });
   }
 }
